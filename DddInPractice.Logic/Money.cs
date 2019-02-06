db6b676a036083dd7a973aa6752c5f6cfdd04e46
Jake Ryu <jake@moneyboxapp.com>
2019-02-06 17:26:32 +0000

    Complete initial model

--- DddInPractice.Logic/Money.cs
+++ DddInPractice.Logic/Money.cs
@@ -7,31 +7,31 @@ namespace DddInPractice.Logic
         public static readonly Money None = new Money(0, 0, 0, 0,
             0, 0, 0, 0);
 
-        public static readonly Money OnePence = new Money(1, 0, 0, 0,
+        public static readonly Money OnePenny = new Money(1, 0, 0, 0,
             0, 0, 0, 0);
 
         public static readonly Money TwoPence = new Money(0, 1, 0, 0,
             0, 0, 0, 0);
 
-        public static readonly Money fivePence = new Money(0, 0, 1, 0,
+        public static readonly Money FivePence = new Money(0, 0, 1, 0,
             0, 0, 0, 0);
 
-        public static readonly Money tenPence = new Money(0, 0, 0, 1,
+        public static readonly Money TenPence = new Money(0, 0, 0, 1,
             0, 0, 0, 0);
 
-        public static readonly Money twentyPence = new Money(0, 0, 0, 0,
+        public static readonly Money TwentyPence = new Money(0, 0, 0, 0,
             1, 0, 0, 0);
 
-        public static readonly Money fiftyPence = new Money(0, 0, 0, 0,
+        public static readonly Money FiftyPence = new Money(0, 0, 0, 0,
             0, 1, 0, 0);
 
-        public static readonly Money onePound = new Money(0, 0, 0, 0,
+        public static readonly Money OnePound = new Money(0, 0, 0, 0,
             0, 0, 1, 0);
 
-        public static readonly Money twoPound = new Money(0, 0, 0, 0,
+        public static readonly Money TwoPound = new Money(0, 0, 0, 0,
             0, 0, 0, 1);
 
-        public int OnePenceCount { get; }
+        public int OnePennyCount { get; }
         public int TwoPenceCount { get; }
         public int FivePenceCount { get; }
         public int TenPenceCount { get; }
@@ -41,7 +41,7 @@ namespace DddInPractice.Logic
         public int TwoPoundCount { get; }
 
         public Money(
-            int onePenceCount,
+            int onePennyCount,
             int twoPenceCount,
             int fivePenceCount,
             int tenPenceCount,
@@ -50,7 +50,7 @@ namespace DddInPractice.Logic
             int onePoundCount,
             int twoPoundCount)
         {
-            if (onePenceCount < 0)
+            if (onePennyCount < 0)
                 throw new InvalidOperationException();
             if (twoPenceCount < 0)
                 throw new InvalidOperationException();
@@ -67,7 +67,7 @@ namespace DddInPractice.Logic
             if (twoPoundCount < 0)
                 throw new InvalidOperationException();
 
-            OnePenceCount = onePenceCount;
+            OnePennyCount = onePennyCount;
             TwoPenceCount = twoPenceCount;
             FivePenceCount = fivePenceCount;
             TenPenceCount = tenPenceCount;
@@ -82,7 +82,7 @@ namespace DddInPractice.Logic
         public static Money operator +(Money money1, Money money2)
         {
             Money sum = new Money(
-                money1.OnePenceCount + money2.OnePenceCount,
+                money1.OnePennyCount + money2.OnePennyCount,
                 money1.TwoPenceCount + money2.TwoPenceCount,
                 money1.FivePenceCount + money2.FivePenceCount,
                 money1.TenPenceCount + money2.TenPenceCount,
@@ -97,7 +97,7 @@ namespace DddInPractice.Logic
         public static Money operator -(Money money1, Money money2)
         {
             return new Money(
-                money1.OnePenceCount - money2.OnePenceCount,
+                money1.OnePennyCount - money2.OnePennyCount,
                 money1.TwoPenceCount - money2.TwoPenceCount,
                 money1.FivePenceCount - money2.FivePenceCount,
                 money1.TenPenceCount - money2.TenPenceCount,
@@ -109,19 +109,19 @@ namespace DddInPractice.Logic
         }
         protected override bool EqualsCore(Money other)
         {
-            return OnePenceCount == other.OnePenceCount &&
+            return OnePennyCount == other.OnePennyCount &&
                    TwoPenceCount == other.TwoPenceCount &&
                    FivePenceCount == other.FivePenceCount &&
                    TenPenceCount == other.TenPenceCount &&
                    TwentyPenceCount == other.TwentyPenceCount &&
                    FiftyPenceCount == other.FiftyPenceCount &&
-                   OnePenceCount == other.OnePenceCount &&
+                   OnePennyCount == other.OnePennyCount &&
                    TwoPoundCount == other.TwoPoundCount;
         }
 
         protected override int GetHashCodeCore()
         {
-            int hashCode = OnePenceCount;
+            int hashCode = OnePennyCount;
             hashCode = (hashCode * 397) ^ TwoPenceCount;
             hashCode = (hashCode * 397) ^ FivePenceCount;
             hashCode = (hashCode * 397) ^ TenPenceCount;
--- DddInPractice.Logic/SnackMachine.cs
+++ DddInPractice.Logic/SnackMachine.cs
@@ -1,7 +1,35 @@
+using System;
+using System.Linq;
+using static DddInPractice.Logic.Money;
+
 namespace DddInPractice.Logic
 {
     public class SnackMachine : Entity
     {
-        
+        public Money MoneyInside { get; private set; }
+        public Money MoneyInTransition { get; private set; }
+
+        public void InsertMoney(Money money)
+        {
+            Money[] conins =
+            {
+                OnePenny, TwoPence, FivePence, TenPence, TwoPence, FiftyPence, OnePound, TwoPound
+            };
+            if(!conins.Contains(money))
+                throw new InvalidOperationException();
+
+            MoneyInTransition += money;
+        }
+
+        public void ReturnMoney()
+        {
+            MoneyInTransition = None;
+        }
+
+        public void BuySnack()
+        {
+            MoneyInside += MoneyInTransition;
+            MoneyInTransition = None;
+        }
     }
 }
